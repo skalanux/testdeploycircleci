4aa2f54ed47d4c9e193fd5850a8d9461f5c031e9
Juan Manuel Schillaci <jmschillaci@gmail.com>
2015-01-17 15:05:40 -0300

    Added templates for aws deployment

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,10 @@ EB_BUCKET=hello-bucket
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+
+sed "s/<EMAIL>/$DOCKER_EMAIL/;s/<AUTH>/$DOCKER_AUTH/" < .dockercfg.template > ~/.dockercfg
+aws s3 cp ~/.dockercfg s3://$EB_BUCKET/dockercfg
+
 aws elasticbeanstalk create-application-version --application-name hello \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
