adf2bdaf14e460c80e703e23e818519a6a260a07
Juan Manuel Schillaci <jmschillaci@gmail.com>
2015-01-17 17:10:51 -0300

    Updated bucket name

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ SHA1=$1
 docker push deveska/qwil-deploy:$HA1
 
 # Create new Elastic Beanstalk version
-EB_BUCKET=hello-bucket
+EB_BUCKET=hello-bucket-ska
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
